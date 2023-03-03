ccafd10cf025f93e972784abd46af5632b93ad48
Mukesh Kumar <97693278+Mukesh1848@users.noreply.github.com>
2023-03-03 21:18:59 +0530

    Create bubble sort.cpp

---
+++ bubble sort.cpp
@@ -0,0 +1,43 @@
+//  first method 
+
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+  
+        // Last i elements are already in place
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+}
+
+
+
+//  second method  
+
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   bool swapped;
+   for (i = 0; i < n-1; i++)
+   {
+     swapped = false;
+     for (j = 0; j < n-i-1; j++)
+     {
+        if (arr[j] > arr[j+1])
+        {
+           swap(&arr[j], &arr[j+1]);
+           swapped = true;
+        }
+     }
+  
+     // IF no two elements were swapped by inner loop, then break
+     if (swapped == false)
+        break;
+   }
+}
+
+
+Worst and Average Case Time Complexity: O(N2). The worst case occurs when an array is reverse sorted.
+Best Case Time Complexity: O(N). The best case occurs when an array is already sorted.
+Auxiliary Space: O(1)
